f23b5a8289ad05e5274023d209d797a97fa724eb
unw10181 <uchennanwilliams@gmail.com>
2025-12-16 08:27:46 -0500

    import modules

--- src/components/CharacterCounter/CharacterCounter.tsx
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -1,14 +1,12 @@
-import type { CharacterCounterProps } from "../../types";
-import React from "react";
+import type { CharacterCounterProps, TextStats } from "../../types";
+import React, { useState } from "react";
+import { TextInput } from "../TextInput/TextInput";
+import { StatDisplay } from "../StatsDisplay/StatsDisplay";
 
 export const CharacterCounter: React.FC<CharacterCounterProps> = ({
-  minWords,
-  maxWords,
+  minWords = 0,
+  maxWords = Infinity,
   targetReadingTime,
 }) => {
-  return (
-    <div className="">
-
-    </div>
-  )
+  return <div className=""></div>;
 };
